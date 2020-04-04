f0f52ab275a393669039b8dc541085c560383cfd
Neetx <neetx@protonmail.com>
2020-04-05 01:47:24 +0200

    Beautify
    Intendation corrections

--- guile-hexdump.scm
+++ guile-hexdump.scm
@@ -25,77 +25,77 @@
 
 ;-----------------------------------------------------------------------------------------------------------------------------
 (define bytevector_to_hex_list			   ;INPUT:bytevector -> OUTPUT: List of string, each one is a 16 bytes hex
-	(lambda (v)
-			(map 
-				(lambda(x)
-					(format #f "~2,,,'0@a" (number->string x 16))
-				)
-				(bytevector->u8-list v))))
+    (lambda (v)
+        (map 
+            (lambda(x)
+                (format #f "~2,,,'0@a" (number->string x 16))
+            )
+            (bytevector->u8-list v))))
 
 (define bytevector_to_ascii_list  			;INPUT:bytevector -> OUTPUT: List of string, each one is an ascii char
-	(lambda (v)
-			(map
-				(lambda(x)
-					(if (and (> x 32) (<= x 126))
-						(string (integer->char x))
-						"."))
-				(bytevector->u8-list v))))
+    (lambda (v)
+        (map
+            (lambda(x)
+                (if (and (> x 32) (<= x 126))
+                    (string (integer->char x))
+                    "."))
+                (bytevector->u8-list v))))
 ;-------------------------------------------------------------------------------------------------------------------------------
 
 (define bytevector_to_hex_string   			;INPUT: bytevector -> OUTPUT: one string of hex  eg."FF FF FF FF"
-	(lambda (v)
-		(string-join (bytevector_to_hex_list v))))
+    (lambda (v)
+        (string-join (bytevector_to_hex_list v))))
 
 (define bytevector_to_ascii_string 			;INPUT; bytevector -> OUTPUT: one string of ascii char eg. "q w e r t y"
-	(lambda (v)
-		(string-join (bytevector_to_ascii_list v))))
+    (lambda (v)
+        (string-join (bytevector_to_ascii_list v))))
 ;--------------------------------------------------------------------------------------------------------------------------------
 
 (define printhexascii
-	(lambda (file l)
-		(define i (get-bytevector-n file 16))
-		(if (eof-object? i)
-			1
-			(begin
-				(display (format #f "~8,,,'0@a" (number->string l 16)))
-				(display "  ")
-				(display (bytevector_to_hex_string i))
-				(if (< (bytevector-length i) 16) (display (format #f "~v{~A~:*~}" (*(- 16 (bytevector-length i)) 3) '(" "))))  ;last raw format handling
-				(display "  |")
-				(display (string-delete #\space (bytevector_to_ascii_string i)))
-				(display "|")
-				(newline)
-			(printhexascii file (+ l 1))))))
+    (lambda (file l)
+        (define i (get-bytevector-n file 16))
+        (if (eof-object? i)
+            1
+            (begin
+                (display (format #f "~8,,,'0@a" (number->string l 16)))
+                (display "  ")
+                (display (bytevector_to_hex_string i))
+                (if (< (bytevector-length i) 16) (display (format #f "~v{~A~:*~}" (*(- 16 (bytevector-length i)) 3) '(" "))))  ;last raw format handling
+                (display "  |")
+                (display (string-delete #\space (bytevector_to_ascii_string i)))
+                (display "|")
+                (newline)
+                (printhexascii file (+ l 1))))))
 
 (define help
-	(lambda ()
-		(display "Usage: hexdump <filename>\n")))
+    (lambda ()
+        (display "Usage: hexdump <filename>\n")))
 
 (define-syntax try
-  (syntax-rules (catch)
-    ((_ body (catch catcher))
-     (call-with-current-continuation
-      (lambda (exit)
-        (with-exception-handler
-         (lambda (condition)
-           catcher
-           (exit condition))
-         (lambda () body)))))))
+    (syntax-rules (catch)
+        ((_ body (catch catcher))
+            (call-with-current-continuation
+                (lambda (exit)
+                    (with-exception-handler
+                        (lambda (condition)
+                            catcher
+                            (exit condition))
+                        (lambda () body)))))))
 
 (define input (program-arguments)) ;TODO: Argparse
 
 (if (<= (length input) 1)
-	(begin
-		(display "ERROR: Please insert a file name.\n")
-		(help)))
+    (begin
+        (display "ERROR: Please insert a file name.\n")
+        (help)))
 
 (define file (list-ref input 1))
 
 (try 
-	(begin 
-     	(define binaryfile (open-file file "rb"))
+    (begin 
+        (define binaryfile (open-file file "rb"))
         (printhexascii binaryfile 0))
-   	(catch
-   		(begin
-   			(display "ERROR: File not found\n")
-   			(help))))
+    (catch
+        (begin
+            (display "ERROR: File not found\n")
+            (help))))
